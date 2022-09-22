6a74d92858efad964106eaa2a17062412deb9b12
ZIHCO <chizobajames21@gmail.com>
2022-09-22 04:03:09 +0000

    More if...elif...

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if (number >= 0):
     if (number % 10) > 5:
         print(f"Last digit of {number:d} is {(number % 10):d}"
               " and is greater than 5")
-    elif (number % 10) <= 5 & (number % 10) > 0:
+    elif (number % 10) <= 5 & (number % 10) != 0:
         print(f"Last digit of {number:d} is {(number % 10):d}"
               " and is less than 6 and not 0")
     elif number % 10 == 0:
@@ -14,7 +14,7 @@ else:
     if (-1 * number) % 10 > 5:
         print(f"Last digit of {number:d} is {((-1 * number) % 10):d}"
               " and is greater than 5")
-    elif (-1 * number) % 10 <= 5 & (-1 * number) % 10 > 0:
+    elif (-1 * number) % 10 <= 5 & (-1 * number) % 10 != 0:
         print(f"Last digit of {number:d} is {((-1 * number) % 10):d}"
               " and is less than 6 and not 0")
     elif (-1 * number) % 10 == 0:
