0778d6dcf354d4c4048d5fe869a4faa532e4d364
ZIHCO <chizobajames21@gmail.com>
2022-09-22 04:11:56 +0000

    More if...elif...

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,17 +5,17 @@ if (number >= 0):
     if (number % 10) > 5:
         print(f"Last digit of {number:d} is {(number % 10):d}"
               " and is greater than 5")
-    elif (number % 10) <= 5 & (number % 10) != 0:
+    elif (number % 10) <= 5 & (number % 10) > 0:
         print(f"Last digit of {number:d} is {(number % 10):d}"
               " and is less than 6 and not 0")
     elif number % 10 == 0:
         print(f"Last digit of {number:d} is {(number % 10):d} and is 0")
 else:
     if (-1 * number) % 10 > 5:
-        print(f"Last digit of {number:d} is {((-1 * number) % 10):d}"
+        print(f"Last digit of {number:d} is {((-1 * number) % 10) * (-1):d}"
               " and is greater than 5")
-    elif (-1 * number) % 10 <= 5 & (-1 * number) % 10 != 0:
-        print(f"Last digit of {number:d} is {((-1 * number) % 10):d}"
+    elif (-1 * number) % 10 <= 5 & (-1 * number) % 10 > 0:
+        print(f"Last digit of {number:d} is {((-1 * number) % 10) * (-1):d}"
               " and is less than 6 and not 0")
     elif (-1 * number) % 10 == 0:
         print(f"Last digit of {number:d} is {((-1 * number) % 10):d} and is 0")
