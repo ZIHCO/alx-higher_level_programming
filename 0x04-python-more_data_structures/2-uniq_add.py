47f4a0120d6e011af6c1a2a5c7a44f3c8749cb0c
ZIHCO <chizobajames21@gmail.com>
2022-10-01 03:43:54 +0000

    Add unique integers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,11 +4,8 @@ def uniq_add(my_list=[]):
     copy_list.sort()
     sum = 0
     for i in range(0, len(my_list)):
-        if i + 1 < len(my_list):
-            if copy_list[i] != copy_list[i + 1]:
-                sum = sum + copy_list[i]
+        if i + 1 < len(my_list) and copy_list[i] != copy_list[i + 1]:
+            sum = sum + copy_list[i]
     if copy_list[len(my_list) - 1] != copy_list[len(my_list) - 2]:
         sum += copy_list[len(my_list) - 1]
     return sum
-
-        
