c9138e8a64f68b178da75f0a9f508ef2b6b87114
ZIHCO <chizobajames21@gmail.com>
2022-10-01 03:58:22 +0000

    sum unquely

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,10 +3,12 @@ def uniq_add(my_list=[]):
     copy_list = my_list.copy()
     copy_list.sort()
     length = len(my_list)
-    sum = copy_list[0]
-    for i in range(0, length):
-        if copy_list[i] != copy_list[i - 1] and i != 0 and i != length - 1:
-            sum = sum + copy_list[i]
-    if copy_list[length - 1] != copy_list[length - 2]:
-        sum += copy_list[length - 1]
+    sum = 0
+    if length > 0:
+        sum = copy_list[0]
+        for i in range(0, length):
+            if copy_list[i] != copy_list[i - 1] and i != 0 and i != length - 1:
+                sum = sum + copy_list[i]
+        if copy_list[length - 1] != copy_list[length - 2]:
+            sum += copy_list[length - 1]
     return sum
