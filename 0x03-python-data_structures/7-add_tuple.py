19dfbe4b1e699836fcff57a8fda77a68056b4ea7
ZIHCO <chizobajames21@gmail.com>
2022-10-02 19:42:30 +0000

    tuple manipulation

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,18 +2,27 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 2 and len(tuple_b) == 2:
         sum_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) >= 1 and len(tuple_b) >= 1:
-        if len(tuple_a) == 2:
+    elif len(tuple_a) == 2 or len(tuple_b) == 2:
+        if len(tuple_a) == 2 and len(tuple_b) == 1:
             sum_tuple = tuple_a[0] + tuple_b[0], tuple_a[0]
-        if len(tuple_b) == 2:
+        if len(tuple_b) == 2 and len(tuple_a) == 1:
             sum_tuple = tuple_a[0] + tuple_b[0], tuple_b[1]
-        if len(tuple_a) == 1 and len(tuple_b) == 1:
-            sum_tuple = tuple_a[0] + tuple_b[0],
-    elif len(tuple_a) >= 0 or len(tuple_b) >= 0:
         if len(tuple_a) == 0:
             sum_tuple = tuple_b
         if len(tuple_b) == 0:
             sum_tuple = tuple_a
-        if len(tuple_a) == 0 and len(tuple_b) == 0:
-            sum_tuple = (0, 0)
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        sum_tuple = tuple_a[0] + tuple_b[0],
+    elif len(tuple_a) == 1 or len(tuple_b) == 1:
+        if len(tuple_a) == 0:
+            sum_tuple = tuple_b
+        if len(tuple_b) == 0:
+            sum_tuple = tuple_a
+    elif len(tuple_a) == 0 or len(tuple_b) == 0:
+        if len(tuple_a) == 0:
+            sum_tuple = tuple_b
+        if len(tuple_b) == 0:
+            sum_tuple = tuple_a
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+        sum_tuple = (0, 0)
     return sum_tuple
