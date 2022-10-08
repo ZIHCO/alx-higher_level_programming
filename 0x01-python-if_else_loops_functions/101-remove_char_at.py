9ef2df905cb9cb538b8fbd6f05869896b829f57e
ZIHCO <chizobajames21@gmail.com>
2022-10-08 21:13:20 +0000

    fixed comment

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,19 @@
+#!/usr/bin/python3
 def remove_char_at(str, n):
+    """a function that creates a copy of the string,
+    removing the character at the position n
+    (not the Python way, the “C array index”).
+
+    parameters:
+    ----------
+    str: a string argument
+    n: integer argument, position to ommit
+
+    Returns:
+    --------
+    a copy with the char at index n ommitted
+    """
+
     i = 0
     newstr = ""
 
