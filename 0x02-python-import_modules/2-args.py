84725dae712dd81a1dd10d947b24119223d8238f
ZIHCO <chizobajames21@gmail.com>
2022-10-10 11:04:27 +0000

    command line argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import sys
-if __name__== "__main__":
+if __name__ == "__main__":
+    """prints the number of and the list of its arguments.
+    Takes commandline argument.
+    """
     argc = len(sys.argv)
     if (argc == 2):
         print("1 argument:\n{:d}: {:s}".format((argc - 1), sys.argv[1]))
