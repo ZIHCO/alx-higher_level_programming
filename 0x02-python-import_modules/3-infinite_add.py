8d78aa6d1db7c0e56edaa520562f671d73f9c8e9
ZIHCO <chizobajames21@gmail.com>
2022-10-10 17:47:21 +0000

    adding large numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    total = 0;
+    for i in range(1, argc):
+        total += int(sys.argv[i])
+    print("{:d}".format(total))
+     
