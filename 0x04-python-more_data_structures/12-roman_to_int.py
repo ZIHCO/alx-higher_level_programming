78a1512dd72d9ddd7f5949dfdaf02552a73f183a
ZIHCO <chizobajames21@gmail.com>
2022-10-17 13:11:45 +0000

    Roman number converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not roman_string) or type(roman_string)  != str:
+    if (not roman_string) or type(roman_string) != str:
         return 0
 
-    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    roman_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10,
+                  'V': 5, 'I': 1}
     sum_num = 0
     num = 0
     i = 0
