31ad1a7b55b621f74249a42257cab783b958fa38
ZIHCO <chizobajames21@gmail.com>
2022-10-18 17:20:46 +0000

    __init__ an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Class
+
+
+Private instance attribute: size
+
+
+"""
+
+
+class Square:
+    """Square Class
+
+    Define a square by private attribute: size
+
+    """
+
+    def __init__(self, size):
+        pass
