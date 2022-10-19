93f589a60f740e4f3e65242489aaa16b609dbf58
ZIHCO <chizobajames21@gmail.com>
2022-10-19 12:55:21 +0000

    Get the area...

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Square Class
+
+
+Private instance attribute: size
+
+
+"""
+
+
+class Square:
+    """Square Class
+
+    Attributes:
+        size: ...
+
+    """
+
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """area
+
+        Returns:
+            the current square area
+
+        """
+        return self.__size**2
