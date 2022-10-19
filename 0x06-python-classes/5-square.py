13dc0988f24bb30797797e05bbc7ac7dbb7b40be
ZIHCO <chizobajames21@gmail.com>
2022-10-19 14:03:46 +0000

    Print the square...

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Square Class
+
+
+Private instance attribute: size
+
+
+"""
+
+
+class Square:
+    """Square Class
+
+    Attributes:
+        size: ...
+
+    """
+
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area
+
+        Returns:
+            the current square area
+
+        """
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        if self.__size:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
