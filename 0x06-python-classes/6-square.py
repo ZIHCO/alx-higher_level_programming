0f0ee77f10ff59fdb97d4a11f5c98e9b2c245caf
ZIHCO <chizobajames21@gmail.com>
2022-10-19 17:09:55 +0000

    fixed position...

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,8 @@ class Square:
            or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2"
                             " positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """area
