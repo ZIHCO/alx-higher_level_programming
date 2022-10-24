a33646d71cff79c9872ce5863541a6e7ea7f6f8c
ZIHCO <chizobajames21@gmail.com>
2022-10-24 16:23:43 +0000

    Trying to pass two failing check

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,8 +26,5 @@ def add_integer(a, b=98):
             raise TypeError("a must be an integer")
         if type(b) is not int and type(b) is not float:
             raise TypeError("b must be an integer")
-        if type(a) is float or type(b) is float:
-            a = int(a)
-            b = int(b)
-        return a + b
+        return int(a) + int(b)
     raise TypeError("a must be an integer")
