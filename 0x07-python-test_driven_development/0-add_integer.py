62f750e58d90eae53c51278715042ac9373201af
ZIHCO <chizobajames21@gmail.com>
2022-10-24 15:28:57 +0000

    Finally, trying TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This is the "0-add_integer" module.
+
+This module has one function, add_integer(). For example,
+
+>>> add_integer(4, 5)
+9
+"""
+
+def add_integer(a, b=98):
+    """add_integer
+
+    sum two int/float value
+
+    Attributes:
+        a and b are integer or float.
+
+    Return:
+        the sum of a and b, where a and b can be 
+        integers or floatand integer.
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float or type(b) is float:
+        a = int(a)
+        b = int(b)
+    return a + b
