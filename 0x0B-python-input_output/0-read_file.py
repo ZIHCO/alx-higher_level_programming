e7bd3d514c44825d53ea4b2a4a80e6423032b2f4
ZIHCO <chizobajames21@gmail.com>
2022-10-25 11:47:57 +0000

    read...

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -18,4 +18,6 @@ def read_file(filename=""):
     """
 
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read(), end="")
+        line = f.read()
+        print(line, end="")
+        return line
