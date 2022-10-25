cdf2933dabcfeae22a2a6aa9e1d397723d4064b5
ZIHCO <chizobajames21@gmail.com>
2022-10-25 11:29:26 +0000

    Read module

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -19,5 +19,4 @@ def read_file(filename=""):
 
     if (filename):
         with open(filename, 'r', encoding="utf-8") as f:
-            for line in f:
-                print(line, end="")
+            print(f.read(), end="")
