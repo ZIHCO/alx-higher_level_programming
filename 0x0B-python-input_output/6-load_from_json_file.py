b8085f2b41f28dde79d08d8c7dc1770ed7e1176c
ZIHCO <chizobajames21@gmail.com>
2022-10-25 20:55:32 +0000

    get object  from file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+"6-load_from_json_file" module
+contains the "load_from_json_file" function
+"""
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file - that creates an Object from a “JSON file”
+
+    Attributes:
+        filename - the json file to create object from
+
+    Return:
+        void
+    """
+    import json
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(f)
