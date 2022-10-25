bd1d95edcb48ace084a97424347c90af4481c2c9
ZIHCO <chizobajames21@gmail.com>
2022-10-25 09:41:50 +0000

    Writing to text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+"1-write_file" module
+Contains the function "write_file"
+"""
+
+def write_file(filename="", text=""):
+    """
+    write_file:
+        writes a string to a text file (UTF8)
+
+    Attributes:
+        takes two argument filename, and the text string to be written
+
+    Return:
+        the number of characters written
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        num_char = f.write(text)
+        return num_char
