00f67b8b6c48f4ae407befb5f0d95ca3ddf8a8c3
ZIHCO <chizobajames21@gmail.com>
2022-10-26 11:22:05 +0000

    Building a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module uses sys, 5-save_to_json_file, 6-load_from_json_file
+modules
+"""
+
 import sys
 save_to = __import__('5-save_to_json_file').save_to_json_file
 load_from = __import__('6-load_from_json_file').load_from_json_file
