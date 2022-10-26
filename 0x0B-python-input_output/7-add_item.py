339b0ae0063004a08767d8f8f8d824dec81edcba
ZIHCO <chizobajames21@gmail.com>
2022-10-26 11:13:32 +0000

    Building a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,10 @@ import sys
 save_to = __import__('5-save_to_json_file').save_to_json_file
 load_from = __import__('6-load_from_json_file').load_from_json_file
 
-
-"""
-This script adds all commandline argument to a LIST
-"""
 if __name__ == "__main__":
+    """
+    This script adds all commandline argument to a LIST
+    """
     obj_list = []
     for i in range(1, len(sys.argv)):
         obj_list.append(sys.argv[i])
