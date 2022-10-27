13903ca8cf1e643e0573a04991b85d3920299429
ZIHCO <chizobajames21@gmail.com>
2022-10-27 16:10:35 +0000

    Chained my if/else

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,9 +31,10 @@ class Rectangle:
         """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self, value):
@@ -45,6 +46,7 @@ class Rectangle:
         """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
