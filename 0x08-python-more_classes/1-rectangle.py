a88ee1b5d873bdce87ab3a0ec19980f70c6eb68d
ZIHCO <chizobajames21@gmail.com>
2022-10-28 05:50:29 +0000

    self.__field must exist

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,12 +29,14 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """width setter"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
+        if self.__width:
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
             self.__width = value
+        else:
+            self._width = 0
 
     @property
     def height(self, value):
@@ -44,9 +46,11 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
+        if self.__height:
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
             self.__height = value
+        else:
+            self.__height = 0
