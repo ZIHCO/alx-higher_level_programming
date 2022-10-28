76a77e227e68058aa392b12bafd605a5f7a6b96e
ZIHCO <chizobajames21@gmail.com>
2022-10-28 06:33:21 +0000

    Permutation...

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,14 +17,14 @@ class Rectangle:
         """
         Initialises the private height and width attributes
         """
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if type(width) is not int:
             raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
@@ -36,10 +36,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """width setter"""
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if type(value) is not int:
             raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -50,8 +50,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
-        if value < 0:
-            raise ValueError("height must be >= 0")
         if type(value) is not int:
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
