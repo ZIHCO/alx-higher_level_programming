c0f3a754ae895aa6b4e7dc4ec9f702aac50f35df
ZIHCO <chizobajames21@gmail.com>
2022-11-01 22:13:43 +0000

    done

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -24,8 +24,8 @@ class Node:
         if type(data) is not int:
             raise TypeError("data must be an integer")
         self.__data = data
-        if (type(next_node) is not None) and (type(next_node) is not type(next_node)):
-            raise TypeError("next_node must be a Node object")
+        """if (type(next_node) is not None) and (type(next_node) is not type(self)):
+            raise TypeError("next_node must be a Node object")"""
         self.__next_node = next_node
 
     @property
@@ -48,8 +48,8 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """setter for next_node"""
-        if (type(value) is not None) and (type(value) is not type(next_node)):
-            raise TypeError("next_node must be a Node object")
+        """if (type(value) is not None) and (type(value) is not type(self)):
+            raise TypeError("next_node must be a Node object")"""
         self.__next_node = value
 
 
@@ -60,15 +60,43 @@ class SinglyLinkedList:
     Attributes:
         head - the head of the singly linked list
     """
+
     def __init__(self):
         """instantiation of an object"""
-        pass
+        self.__head = None
 
     def sorted_insert(self, value):
         """Instance method that inserts a new Node"""
-        """if SinglyLinkedList() == 0:"""
-        __head = Node(value)
-        return __head
-    """def __repr__(self):
-        ""printable list""
-        return str(__head)"""
+        """node = Node(value)
+        if SinglyLinkedList.__count_insert == 1:
+            self.__head = node
+        return self.__head.data"""
+        node = Node(value)
+        if self.__head:
+            if self.__head.data >= value:
+                node.next_node = self.__head
+                self.__head = node
+            else:
+                tmp = self.__head
+                prev = self.__head
+                while tmp.next_node:
+                    tmp = tmp.next_node
+                    if (tmp.data < value) and prev.data < value:
+                        prev = prev.next_node
+                    elif (tmp.data >= value) and prev.data <= value:
+                        prev.next_node = node
+                        node.next_node = tmp
+                        break
+                else:
+                    prev.next_node = node
+        else:
+            self.__head = node
+        return self.__head
+        
+    def __str__(self):
+        """printable list"""
+        node = self.__head
+        while node.next_node:
+            print(node.data)
+            node = node.next_node
+        return str(node.data)
