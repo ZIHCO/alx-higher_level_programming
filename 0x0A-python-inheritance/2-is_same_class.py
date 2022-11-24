553b86f35cfad7e81505520e17a77e4741830d31
ZIHCO <chizobajames21@gmail.com>
2022-11-24 11:28:53 +0000

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+2-is_same_class module, contain a function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns a bool if obj is an instance of a_class"""
+    if type(obj) is not None and type(a_class) is not None:
+        if isinstance(obj, a_class):
+            return True
+        return False
