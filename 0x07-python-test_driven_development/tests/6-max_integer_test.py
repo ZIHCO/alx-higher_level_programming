deb2e68bfe7dc83d0c3c7218c8d3b2ead710b8dc
ZIHCO <chizobajames21@gmail.com>
2022-11-26 05:41:13 +0000

    first unittestcase

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,7 +10,3 @@ class TestMaxInteger(unittest.TestCase):
         """argument test"""
         self.assertEqual(max_integer([2, 4, 5, 1]), 5)
         self.assertEqual(max_integer(), None)
-
-    def test_type(self):
-        """argument type"""
-        self.assertRaises(TypeError, max_integer, "3") 
