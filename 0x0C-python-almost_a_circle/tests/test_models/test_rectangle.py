f64eadbc516789ddd81663d3d7acf36c1ecd0245
ZIHCO <chizobajames21@gmail.com>
2022-11-27 14:06:54 +0000

    raises assertions

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,3 +11,15 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(Rectangle(12, 2, 0, 3, 23).id, 23)
         self.assertEqual(Rectangle(2, 20).id, 1)
         self.assertEqual(Rectangle(3, 10).id, 2)
+
+    def test_validator(self):
+        """Test for the setter and getter validator"""
+        r = Rectangle(10, 2)
+        with self.assertRaises(ValueError):
+            r.width = -10
+        with self.assertRaises(TypeError):
+            r.height = "10"
+        with self.assertRaises(TypeError):
+            r.x = {}
+        with self.assertRaises(ValueError):
+            r.y = -1
