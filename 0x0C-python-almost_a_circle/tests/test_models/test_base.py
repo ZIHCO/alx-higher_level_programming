4258ef5313f951f3c8f7fe23a5e04b5ea32a5427
ZIHCO <chizobajames21@gmail.com>
2022-11-27 15:30:02 +0000

    base test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,11 +6,14 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """Testcase for Base class"""
 
+    def setUp(self):
+        """set up cases"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(12)
+
     def test__id(self):
         """Test the __init__ method"""
-        self.assertEqual(Base().id, 1)
-        self.assertEqual(Base().id, 2)
-        self.assertEqual(Base(12).id, 12)
-
-if __name__ == '__main__':
-        unittest.main()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 12)
