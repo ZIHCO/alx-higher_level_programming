9c31f24401fad675a77b04ad97a413b2cb3591b8
ZIHCO <chizobajames21@gmail.com>
2022-11-27 12:17:54 +0000

    Raise assertions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,7 +37,7 @@ class Rectangle(Base):
         """set width"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        if width <= 0:
             raise ValueError("width must be >= 0")
         self.__width = width
 
@@ -51,7 +51,7 @@ class Rectangle(Base):
         """set height"""
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if height <= 0:
             raise ValueError("height must be >= 0")
         self.__height = height
 
