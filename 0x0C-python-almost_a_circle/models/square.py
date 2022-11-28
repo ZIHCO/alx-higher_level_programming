9632ab3f3ea261aad1fb0e729bb81dd2a891584a
ZIHCO <chizobajames21@gmail.com>
2022-11-28 12:49:42 +0000

    squares are special rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """class constructor"""
         Rectangle.__init__(self, size, size, x, y, id)
-  
+
     def __str__(self):
         """override the __str__"""
         return ("[Square] " + "(" + str(self.id) + ") " + str(self.x)
