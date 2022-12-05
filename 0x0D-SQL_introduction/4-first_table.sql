387ac04fbd33004a6c98562b9b0d5cf5c3612f8c
ZIHCO <chizobajames21@gmail.com>
2022-12-05 08:22:59 +0000

    done

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table called first-table in current database
 -- CREATE TABLE creates a table
-CREATE TABLE IF EXISTS first_name (id INT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
