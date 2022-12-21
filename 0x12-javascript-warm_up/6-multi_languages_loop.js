caed63cb1b02a080315362aaffa80ecd18445bf8
ZIHCO <chizobajames21@gmail.com>
2022-12-21 02:31:18 +0000

    loop and print

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * This module print 3 lines of string using
+ * an array, and a loop
+ */
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
+}
