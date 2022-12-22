18d3fb0f1d523e8f939fadf63494143932a5cdd9
ZIHCO <chizobajames21@gmail.com>
2022-12-22 04:38:21 +0000

    inherit from same name super class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+ * This module contains a Square subclass
+ */
+
+const SquareP = require('./5-square');
+module.exports = class Square extends SquareP {
+  /* The constructor  takes 2 args w and h
+   * print method
+   */
+
+  constructor (size) {
+    super();
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        let str = '';
+        for (let j = 0; j < this.size; j++) {
+          str = str + 'c';
+        }
+        console.log(str);
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        let str = '';
+        for (let j = 0; j < this.size; j++) {
+          str = str + 'X';
+        }
+        console.log(str);
+      }
+    }
+  }
+};
