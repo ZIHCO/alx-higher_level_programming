0bfc6e77d34b52d171b98a41dc26f7f9cca43d2d
ZIHCO <chizobajames21@gmail.com>
2022-12-22 03:09:31 +0000

    used a constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * This module contains a Rectangle class
+ */
+
+module.exports = class Rectangle {
+  // The constructor  takes 2 args w and h
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
