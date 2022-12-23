52cb04c487c6d628e0f23aa204a5a1fecb072961
ZIHCO <chizobajames21@gmail.com>
2022-12-23 03:01:13 +0000

    included port, and host

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 MySQLdb = __import__('MySQLdb')
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states""")
     rows = c.fetchall()
