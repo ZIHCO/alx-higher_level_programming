f05c71ce74570a7a42ce0b7698c898da68ef6d70
ZIHCO <chizobajames21@gmail.com>
2022-12-23 03:04:28 +0000

    fixed a typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@
 """
 
 import sys
-MySQLdb = __import__('MySQLdb')
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
