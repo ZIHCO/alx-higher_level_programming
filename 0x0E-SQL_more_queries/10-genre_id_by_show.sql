a800d4933696ebb9c050cb799f34fca04094990a
ZIHCO <chizobajames21@gmail.com>
2022-12-28 01:51:24 +0000

    fixed typo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- import the DB hbtn_0d_tvshows
 -- lists all shows contained in hbtn_0d_tvshows
 
-SELECT  tv_shows.title, tv_show_genres.genre_id FROM tv_genres join tv_show_genres, tv_shows WHERE genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id
+SELECT  tv_shows.title, tv_show_genres.genre_id FROM tv_genres JOIN tv_show_genres, tv_shows WHERE genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
