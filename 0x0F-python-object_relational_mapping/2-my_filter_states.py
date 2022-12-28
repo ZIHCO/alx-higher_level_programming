0fc2d8606e25e70c180260acab6e1fee29ae2d0c
ZIHCO <chizobajames21@gmail.com>
2022-12-28 02:51:57 +0000

    read extra arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+   This module is script that lists all values states table
+   the database hbtn_0e_0_usa where name matches argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    state_name = sys.argv[4]
+    c = db.cursor()
+    c.execute("""SELECT * FROM states
+              WHERE name = '{}'""".format(state_name))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
