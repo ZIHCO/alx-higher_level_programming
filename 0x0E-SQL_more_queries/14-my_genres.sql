9b44ba985aa6c3b556d496048f64953ebb4da0c2
ZIHCO <chizobajames21@gmail.com>
2022-12-29 12:46:51 +0000

    done

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,8 @@
 -- lists all shows contained in hbtn_0d_tvshows
 
 SELECT tv_genres.name AS 'name'
-	FROM tv_show_genres
-	JOIN tv_genres
-	ON tv_show_genres.genre_id = tv_genres.id
-	WHERE tv_show_genres.show_id = 8
+	FROM tv_genres
+	JOIN tv_show_genres, tv_shows
+	WHERE (genre_id = tv_genres.id AND tv_show_genres.show_id = tv_shows.id)
+	AND tv_shows.title = 'Dexter'
+	ORDER BY name
