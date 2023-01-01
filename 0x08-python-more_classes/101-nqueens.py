1b7a18f6120a6c611279dd9545caabfb8817a633
ZIHCO <chizobajames21@gmail.com>
2023-01-01 10:24:14 +0000

    NQUEEN puzzle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         try:
             N = int(sys.argv[1])
-        except:
+        except Exception:
             print("N must be a number")
             exit(1)
         if N < 4:
