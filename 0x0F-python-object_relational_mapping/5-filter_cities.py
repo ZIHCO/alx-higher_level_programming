7d73a61e0aebce5a4eda4323fbdd4f8c0da584e7
ZIHCO <chizobajames21@gmail.com>
2023-01-10 06:48:39 +0000

    Using logical AND

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module is a script that name of a state
+as an argument and list all cities of the state
+hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(port=3306, user=sys.argv[1], host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    with db.cursor() as c:
+        c.execute("""
+                  SELECT
+                      cities.name
+                  FROM
+                      cities
+                  JOIN
+                      states
+                  WHERE
+                      states.name
+                  LIKE BINARY
+                      %(name)s
+                  AND
+                      state_id = states.id
+                  ORDER
+                      BY
+                  cities.id
+                      ASC
+                  """,
+                  {'name': (sys.argv[4])})
+        rows = c.fetchall()
+        i = 0
+        for row in rows:
+            if i + 1 == len(rows):
+                print(row[0])
+            else:
+                print(row[0] + ",", end='')
+            i += 1
