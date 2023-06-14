4fc6701afa0bd3c4b125cfae872489884521289b
ZIHCO <chizobajames21@gmail.com>
2023-06-14 04:09:58 +0100

    parseInt stringed number

---
+++ 0x12-javascript-warm_up/5-to-integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/*
+ * parseInt()
+ */
+
+if (parseInt(process.argv[2])) {
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
+}
