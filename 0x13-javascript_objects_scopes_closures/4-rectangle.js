86d9b4a5eb28b4bf9d6ebe8e388a5b9b1abe4b83
ZIHCO <chizobajames21@gmail.com>
2023-06-15 02:55:58 +0100

    add 2 more method

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+/*
+ * gate your instance attribute
+ * add a print()  method
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let width = '';
+      for (let j = 0; j < this.width; j++) {
+        width += 'X';
+      }
+      console.log(width);
+    }
+  }
+
+  rotate () {
+    const width = this.width;
+    this.width = this.height;
+    this.height = width;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+};
