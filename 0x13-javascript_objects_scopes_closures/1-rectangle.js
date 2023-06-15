cd272e3f3a8e51aae32aae8fdb35771f0caceec5
ZIHCO <chizobajames21@gmail.com>
2023-06-15 02:19:39 +0100

    update a class with constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * update the rectangle class with a constructor
+ */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
