77fbdde59ef7252dadbab7bd14a427d1e2c734de
ZIHCO <chizobajames21@gmail.com>
2023-06-20 05:48:58 +0100

    sql injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+   This module is script that lists all values states table
+   the database hbtn_0e_0_usa where name matches argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    with db.cursor() as c:
+        state_name = sys.argv[4]
+        c.execute("""
+                SELECT
+                    *
+                FROM
+                    states
+                 WHERE
+                    name
+                LIKE
+                    BINARY
+                %(name)s
+                 ORDER
+                    BY
+                states.id ASC
+                """, {'name': state_name})
+        rows = c.fetchall()
+        for row in rows:
+            print(row)
