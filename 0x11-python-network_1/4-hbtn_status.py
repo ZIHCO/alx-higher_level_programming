6f5655a61e332927e22442c6141a437709040b0a
ZIHCO <chizobajames21@gmail.com>
2023-07-09 10:47:45 +0100

    done

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""script fetches 'https://alx-intranet.hbtn.io/status'
+   using 'urllib'
+"""
+
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    html = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(html) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(utf8_content)))
+        print('\t- content: {_content}'.format(_content=utf8_content))
