411a7f6fc82afd263f70fc89a237871cf167be37
ZIHCO <chizobajames21@gmail.com>
2023-07-09 11:59:51 +0100

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,4 +10,4 @@ from sys import argv
 if __name__ == "__main__":
     payload = dict(email=argv[2])
     r = requests.post(argv[1], payload)
-    print(r.context.decode('utf-8'))
+    print(r.text)
