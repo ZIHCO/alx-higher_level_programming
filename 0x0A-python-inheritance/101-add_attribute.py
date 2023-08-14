c85c1ed678e98997a48f9ab97cacbc6af36d7dc3
ZIHCO <chizobajames21@gmail.com>
2023-08-14 17:50:07 +0100

    try this

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the definition, add_attribute."""
+
+
+def add_attribute(obj, attri, value):
+    """add attribute to an object if possible"""
+    setattr(obj, attri, value)
