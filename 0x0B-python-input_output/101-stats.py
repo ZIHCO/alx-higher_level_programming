639d20c9c523977d3a5e48da7f6ebb6d5ad01418
ZIHCO <chizobajames21@gmail.com>
2023-08-16 18:45:43 +0100

    done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,52 +1,24 @@
 #!/usr/bin/python3
-import sys
+"""This script reads from stdin"""
 
+if __name__ == "__main__":
+    from sys import stdin
 
-def print_info():
-    print('File size: {:d}'.format(file_size))
-
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
-
-
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
-lc = 0
-file_size = 0
-
-try:
-    for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
-
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
-
-        lc += 1
-
-    print_info()
-except KeyboardInterrupt:
-    print_info()
-    raise
+    line = stdin.readline()
+    i = 0
+    line_dict = {"File size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
+                 "403": 0, "404": 0, "405": 0, "500": 0}
+    while line:
+        if i % 10 == 0 and i != 0:
+            print(f"File size: {line_dict['File size']}")
+            list_code = list(sorted(line_dict))[:-1]
+            for item in list_code:
+                if line_dict[item] != 0:
+                    print(f"{item}: {line_dict[item]}")
+            i = 0
+            continue
+        line_list = line.split()
+        line_dict['File size'] += int(line_list[-1])
+        line_dict[line_list[-2]] += 1
+        line = stdin.readline()
+        i += 1
