02c49d115a6a6122379734176146043f0aa1e57a
ZIHCO <chizobajames21@gmail.com>
2023-08-16 11:09:22 +0100

    done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
-"""This module is a script for getting HTTP responses"""
+import sys
 
 
-if __name__ == "__main__":
-    from sys import stdin
+def print_info():
+    print('File size: {:d}'.format(file_size))
 
-    line = stdin.readline()
-    while line != "":
-        result_dict = {"File size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
-                       "403": 0, "404": 0, "405": 0, "500": 0}
-        for i in range(10):
-            make_list = line.split()
-            result_dict["File size"] += int(make_list[-1])
-            result_dict[(make_list[-2])] += 1
-            line = stdin.readline()
-        code_list = list((result_dict))
-        for item in code_list:
-            if result_dict[item] > 0:
-                print(f"{item}: {result_dict[item]}")
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
+
+
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+lc = 0
+file_size = 0
+
+try:
+    for line in sys.stdin:
+        if lc != 0 and lc % 10 == 0:
+            print_info()
+
+        pieces = line.split()
+
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except:
+            pass
+
+        lc += 1
+
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
