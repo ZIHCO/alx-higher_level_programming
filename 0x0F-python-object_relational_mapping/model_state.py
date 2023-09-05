0e9a859f44f64dd616dbdedcef8827014831068f
ZIHCO <chizobajames21@gmail.com>
2023-09-05 21:36:45 +0100

    fine

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containsthe class definition of a State,
+   and an instance Base
+"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class relational mapper"""
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, primary_key=True)
+    name = Column(String(128))
