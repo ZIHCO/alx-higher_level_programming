90fed9e61db40b0845b245741e9676ef877b2431
ZIHCO <chizobajames21@gmail.com>
2023-09-25 18:45:40 +0100

    done

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""My first orm query"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    engine = create_engine(conn, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+        if not session.query(State).order_by(State.id)[0:1]:
+            print("Nothing")
+        else:
+            for state in session.query(State).order_by(State.id)[0:1]:
+                print("{}: {}".format(state.id, state.name))
