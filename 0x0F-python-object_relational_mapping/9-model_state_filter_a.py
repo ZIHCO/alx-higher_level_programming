df0dab8c14b752b7118de5ab3f0f67ca4af60c69
ZIHCO <chizobajames21@gmail.com>
2023-09-25 20:29:54 +0100

    done

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""My first orm query"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    engine = create_engine(conn, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+
+        for state in session.query(State).order_by(State.id).all():
+            if "a" in state.name:
+                print("{}: {}".format(state.id, state.name))
