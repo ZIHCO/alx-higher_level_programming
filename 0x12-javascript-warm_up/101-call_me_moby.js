f4608327ff6f5843bab15d4fa377fd6dfd86b77c
ZIHCO <chizobajames21@gmail.com>
2023-09-26 13:35:46 +0100

    done

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * exportatable function
+ */
+
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+exports.callMeMoby = callMeMoby;
