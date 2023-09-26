c7fdd1ccc047e5d235af9b634ae64b9b2d17a697
ZIHCO <chizobajames21@gmail.com>
2023-09-26 21:50:46 +0100

    done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -5,7 +5,7 @@
 
 const list = require('./100-data').list;
 
-const listIndex = list.map((x) => (x * list.indexOf(x)));
+const listIndex = list.map((x, i) => (x * i));
 
 console.log(list);
 console.log(listIndex);
