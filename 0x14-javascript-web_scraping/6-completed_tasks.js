c26f56838e80a14c48382ac9fc711f10dd9ec33a
ZIHCO <chizobajames21@gmail.com>
2023-09-27 18:32:48 +0100

    done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,6 +8,7 @@ const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
+    return;
   }
   const listUsers = JSON.parse(body);
   const listId = [];
