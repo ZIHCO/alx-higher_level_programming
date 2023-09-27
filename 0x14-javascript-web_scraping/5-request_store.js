27c45ba308c092d2bf8e0fd75e8606c0cffba455
ZIHCO <chizobajames21@gmail.com>
2023-09-27 17:53:31 +0100

    done

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+ * script that gets the contents of a webpage and stores it in a file.
+ */
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFile(filename, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
