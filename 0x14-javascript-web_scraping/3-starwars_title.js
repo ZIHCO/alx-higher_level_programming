dfacf43551c30b5a0119f7f263c6a372d32930e5
ZIHCO <chizobajames21@gmail.com>
2023-09-27 15:38:38 +0100

    done

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * Write a script that display the status code of a GET request.
+ */
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
