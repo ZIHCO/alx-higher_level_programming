054b2409d58d4c669ce49c5a8f235effb9c3595b
ZIHCO <chizobajames21@gmail.com>
2023-09-27 14:26:45 +0100

    done

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * Write a script that reads and prints the content of a file.
+ */
+
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, content) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(content);
+	}
+});
+
