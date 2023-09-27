72511943388521c7ad08d1f421671e75f15507f8
ZIHCO <chizobajames21@gmail.com>
2023-09-27 14:27:15 +0100

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,10 +5,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, content) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(content);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
-
