029272004553ca6224b8b58aba83806ec46ffe46
ZIHCO <chizobajames21@gmail.com>
2023-09-29 11:54:11 +0100

    done

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 
 class State(Base):
     """State class relational mapper"""
-    from relationship_city import City
     __tablename__ = "states"
     id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(128))
-    cities = relationship("City", cascade="all, delete, delete-orphan")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
