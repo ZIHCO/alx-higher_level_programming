32c9ee838854082671ccc706a64c5c178071c20f
ZIHCO <chizobajames21@gmail.com>
2023-10-04 21:28:28 +0100

    done

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+const listMovies = $('#list_movies');
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+
+$.get(url, function (data, statusText) {
+  const listObjs = data.results;
+  for (let count = 0; count < listObjs.length; count++) {
+    const movie = $('<li></li>').text(listObjs[count].title);
+    listMovies.append(movie);
+  }
+});
