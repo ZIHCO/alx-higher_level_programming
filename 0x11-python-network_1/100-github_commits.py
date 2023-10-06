acde00fa6301962ba575ab232865b9d8b6db4a30
ZIHCO <chizobajames21@gmail.com>
2023-10-06 13:01:57 +0100

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes two args, repos_name and owner_name
+   list 10 commits of this repos
+"""
+from sys import argv
+import requests
+
+url = 'https://api.github.com/' + 'repos/' + argv[2] + '/' + argv[1]
+response = requests.get(url).json()
+commit_url = response['commits_url'][:-6]
+
+commits_response = requests.get(commit_url).json()
+for i in range(10):
+    print(f"{commits_response[i]['sha']}: "
+          f"{commits_response[i]['author']['login']}")
